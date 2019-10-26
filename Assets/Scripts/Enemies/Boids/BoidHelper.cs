16492d2cd8f4c00ce023fc2c78fbf30cc3d149bb
Pokoi <jesusferminvillar@gmail.com>
2019-10-26 04:21:06 +0200

    Boid behaviour added

---
+++ Assets/Scripts/Enemies/Boids/BoidHelper.cs
@@ -0,0 +1,39 @@
+﻿//
+// File: BoidHelper.cs
+// Project: HalloUEM2019
+// Description : GameJam Project
+// Author: Original File from Sebastian Lague at https://github.com/SebLague/Boids
+// Modificated by Jesús Fermín Villar Ramírez (pokoidev)
+
+
+// MIT License
+// © Copyright (C) 2019  pokoidev
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BoidHelper {
+
+    const int numViewDirections = 300;
+    public static readonly Vector3[] directions;
+
+    static BoidHelper () {
+        directions = new Vector3[BoidHelper.numViewDirections];
+
+        float goldenRatio = (1 + Mathf.Sqrt (5)) / 2;
+        float angleIncrement = Mathf.PI * 2 * goldenRatio;
+
+        for (int i = 0; i < numViewDirections; i++) {
+            float t = (float) i / numViewDirections;
+            float inclination = Mathf.Acos (1 - 2 * t);
+            float azimuth = angleIncrement * i;
+
+            float x = Mathf.Sin (inclination) * Mathf.Cos (azimuth);
+            float y = Mathf.Sin (inclination) * Mathf.Sin (azimuth);
+            float z = Mathf.Cos (inclination);
+            directions[i] = new Vector3 (x, y, z);
+        }
+    }
+
+}
